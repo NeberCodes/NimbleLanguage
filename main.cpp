99690cb7855e73b47812a70779ff1a0f3def5e26
danielrigby <danielrigby@192.168.0.6>
2014-12-23 08:22:36 +1000

    Skeleton Compiler

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Daniel Rigby
+ *
+ * Created on December 23, 2014, 8:21 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
